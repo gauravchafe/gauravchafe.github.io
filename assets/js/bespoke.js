23d69c44980c4d22c9642ae31f580e760b704d18
gauravchafe <gaurav.chafe@gmail.com>
2024-07-31 20:35:27 +0530

    Changed navbar to offcanvas and added jumbotron

---
+++ assets/js/bespoke.js
@@ -0,0 +1,9 @@
+/* Offcanvas Navbar code starts here*/
+(() => {
+'use strict'
+
+document.querySelector('#navbarSideCollapse').addEventListener('click', () => {
+    document.querySelector('.offcanvas-collapse').classList.toggle('open')
+})
+})()
+/* Offcanvas Navbar code ends here*/
